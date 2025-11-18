22634945c2c28460d978cd3659c8be9c19cbb2c4
OnlyBee <99727319+OnlyBee@users.noreply.github.com>
2025-11-18 15:47:12 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -4,13 +4,11 @@ import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 
 const App: React.FC = () => {
-  const { token } = useAuth(); // lấy token từ AuthContext
+  const { user } = useAuth();
 
   return (
     <div className="min-h-screen bg-gray-900 text-gray-100 font-sans">
-      {/* Nếu có token => đã đăng nhập => hiện Dashboard.
-          Nếu chưa có token => hiện màn Login */}
-      {token ? <Dashboard /> : <Login />}
+      {user ? <Dashboard /> : <Login />}
     </div>
   );
 };
