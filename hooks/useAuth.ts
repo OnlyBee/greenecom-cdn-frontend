4de99775383f01fa4d3dba7c095bdb39f0599b8e
OnlyBee <99727319+OnlyBee@users.noreply.github.com>
2025-11-19 01:19:54 +0700

    Update useAuth.ts

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,12 +1,11 @@
-// src/hooks/useAuth.ts
 
 import { useContext } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
 
 export const useAuth = () => {
-  const ctx = useContext(AuthContext);
-  if (!ctx) {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
     throw new Error('useAuth must be used within an AuthProvider');
   }
-  return ctx;
+  return context;
 };
